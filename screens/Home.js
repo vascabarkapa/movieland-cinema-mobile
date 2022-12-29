760dc0398c722d653e83f4fe2ae203fe5460cc69
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-12-30 00:42:10 +0100

    Restructured pages

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Text, View} from "react-native";
 
-export default function HomePage() {
+const Home = () => {
     return (
         <View>
             <Text>This is Home Page!</Text>
         </View>
     )
 }
+
+export default Home;
