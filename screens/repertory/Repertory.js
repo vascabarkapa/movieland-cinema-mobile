9996790ccdf2a884e34d8e58bd3772d51012772b
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-12-31 00:37:35 +0100

    Added other screens

---
+++ screens/repertory/Repertory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const Repertory = () => {
+    return (
+        <View>
+            <Text>This is Repertory Page!</Text>
+        </View>
+    )
+}
+
+export default Repertory;
