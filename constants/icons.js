f5b73f0fa6130d7120db90826735d5b36d144ae9
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-12-31 14:49:17 +0100

    Added icons

---
+++ constants/icons.js
@@ -0,0 +1,9 @@
+export const home = require('../assets/icons/home.png');
+export const info = require('../assets/icons/info.png');
+export const movieland = require('../assets/icons/movieland_icon.png');
+
+export default {
+    home,
+    info,
+    movieland
+}
