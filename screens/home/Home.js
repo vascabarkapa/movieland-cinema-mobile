6c673dfa35650172b873f83f19471fadc94801ee
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-12-31 15:45:34 +0100

    New Home page

--- screens/home/Home.js
+++ screens/home/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import {Text, View} from "react-native";
+import {ImageBackground, Text, View} from "react-native";
+import {COLORS, images} from "../../constants";
 
 const Home = () => {
     return (
-        <View>
-            <Text>This is Home Page!</Text>
+        <View style={{flex: 1}}>
+            <ImageBackground source={images.cinema} resizeMode='cover' style={{flex: 1, justifyContent: 'center'}}>
+                <Text style={{color: COLORS.white}}>Welcome to our cinema</Text>
+                <Text style={{color: COLORS.white}}>Following the modern times and adapting the film industry to today's
+                    digital devices,
+                    Movieland Cinema offers you unforgettable film experiences.</Text>
+            </ImageBackground>
         </View>
     )
 }
