e96c0ed2edfccf38bca8d1fc92963f8133390812
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-12-31 15:55:53 +0100

    Added text to home page

--- screens/home/Home.js
+++ screens/home/Home.js
@@ -6,8 +6,11 @@ const Home = () => {
     return (
         <View style={{flex: 1}}>
             <ImageBackground source={images.cinema} resizeMode='cover' style={{flex: 1, justifyContent: 'center'}}>
-                <Text style={{color: COLORS.white}}>Welcome to our cinema</Text>
-                <Text style={{color: COLORS.white}}>Following the modern times and adapting the film industry to today's
+                <Text style={{color: COLORS.white, fontSize: 30, fontWeight: 'bold', textAlign: 'center', top: -20}}>Welcome
+                    to our cinema</Text>
+                <Text style={{color: COLORS.white, textAlign: 'center', marginTop: 250}}>Following the modern times and
+                    adapting the
+                    film industry to today's
                     digital devices,
                     Movieland Cinema offers you unforgettable film experiences.</Text>
             </ImageBackground>
