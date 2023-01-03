0cac5a58a8c640dbd79644ceeb71ae541d0f3e86
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2023-01-04 00:33:15 +0100

    Poppins font in movie details

--- screens/repertory/components/MovieDetails.js
+++ screens/repertory/components/MovieDetails.js
@@ -1,12 +1,31 @@
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import repertoryDB from "../../../repertoryDB";
 import {Alert, Image, Text, TextInput, TouchableOpacity, View, StyleSheet} from "react-native";
-import {images} from "../../../constants";
+import {COLORS} from "../../../constants";
+import {useFonts} from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 const MovieDetails = ({route, navigation}) => {
     const [ticketQuantity, setTicketQuantity] = useState(1);
     const {movieId} = route.params;
     const selectedMovie = repertoryDB.find(movies => movies.id === movieId);
+
+    const [fontsLoaded] = useFonts({
+        'Poppins-Regular': require('./../../../assets/fonts/Poppins-Regular.ttf'),
+        'Poppins-Bold': require('./../../../assets/fonts/Poppins-Bold.ttf')
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
 
     const purchase = () => {
         let price = selectedMovie.price * ticketQuantity;
@@ -15,14 +34,18 @@ const MovieDetails = ({route, navigation}) => {
     }
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>{selectedMovie.name}</Text>
-            <Image style={styles.image} source={images.cinema}/>
-            <Text style={styles.description}>{selectedMovie.description}</Text>
+        <View style={styles.container} onLayout={onLayoutRootView}>
+            <Text style={styles.title}>
+                {selectedMovie.name}
+            </Text>
+            <Image style={styles.image} source={selectedMovie.image}/>
+            <Text style={styles.description}>
+                {selectedMovie.description}
+            </Text>
             <View style={styles.purchaseRow}>
                 <Text style={styles.description}>Quantity: </Text>
                 <TextInput style={styles.quantityInput} onChangeText={quantity => setTicketQuantity(quantity)}
-                           value={ticketQuantity} selectTextOnFocus={true} keyboardType='numeric'/>
+                           value={ticketQuantity.toString()} selectTextOnFocus={true} keyboardType='numeric'/>
             </View>
             <Text style={styles.price}>
                 Total Price: ${selectedMovie.price * ticketQuantity}
@@ -44,19 +67,21 @@ const styles = StyleSheet.create({
         flexDirection: 'row'
     },
     button: {
-        borderTopWidth: 1,
-        borderBottomWidth: 1,
-        width: '60%'
+        backgroundColor: COLORS.secondary,
+        width: '40%',
+        borderRadius: 20,
     },
     title: {
-        fontWeight: 'bold',
+        marginTop: 30,
+        fontFamily: 'Poppins-Bold',
         paddingTop: 10,
     },
     image: {
         width: '100%',
-        height: 180,
+        height: 360,
     },
     description: {
+        fontFamily: 'Poppins-Regular',
         textAlignVertical: 'center',
         paddingTop: 5,
         textAlign: 'left',
@@ -66,16 +91,25 @@ const styles = StyleSheet.create({
     price: {
         paddingTop: 5,
         paddingBottom: 10,
+        fontFamily: 'Poppins-Bold',
     },
     buttonText: {
+        fontFamily: 'Poppins-Bold',
+        color: COLORS.white,
+        textTransform: 'uppercase',
         textAlign: 'center',
         padding: 5,
     },
     quantityInput: {
+        fontFamily: 'Poppins-Regular',
         borderWidth: 1,
-        height: 38,
-        width: 40,
-        marginLeft: 25
+        borderColor: COLORS.secondary,
+        borderRadius: 10,
+        height: 32,
+        width: 80,
+        marginLeft: 25,
+        paddingLeft:10,
+        paddingTop:2
     }
 })
 
