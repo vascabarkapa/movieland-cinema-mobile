7e4e4f72b7fea0f942756d968c37b4679ad0e2c1
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2023-01-03 23:42:49 +0100

    Added Dummy movies

--- repertoryDB.js
+++ repertoryDB.js
@@ -1,45 +1,47 @@
+import {images} from "./constants";
+
 const repertory = [
     {
         id: 1,
         name: 'The Big Short',
-        image: '',
-        genre: 'Drama',
-        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-        duration: '1h30min',
+        image: images.dummyTheBigShort,
+        genre: 'Comedy, Drama',
+        description: 'In 2006-2007 a group of investors bet against the US mortgage market. In their research, they discover how flawed and corrupt the market is.',
+        duration: '2h 10min',
         direction: 'Vasilije Čabarkapa',
         actors: 'Neven Ivanović, Marko Marković',
-        rating: 8.2,
-        dateTime: '02.01.2023. 19:30',
+        rating: 7.8,
+        dateTime: '02.01.2023. 19:30h',
         numberOfTickets: 220,
         price: 5.00
     },
     {
         id: 2,
-        name: 'The Big Short',
-        image: '',
+        name: 'Rocky III',
+        image: images.dummyRockyIII,
         genre: 'Drama',
-        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-        duration: '1h30min',
+        description: 'Rocky faces the ultimate challenge from a powerful new contender, and must turn to a former rival to help regain his throne as the undisputed fighting champion.',
+        duration: '1h 39min',
         direction: 'Vasilije Čabarkapa',
         actors: 'Neven Ivanović, Marko Marković',
-        rating: 8.2,
-        dateTime: '02.01.2023. 19:30',
+        rating: 6.8,
+        dateTime: '03.01.2023. 20:00h',
         numberOfTickets: 220,
-        price: 5.00
+        price: 3.50
     },
     {
         id: 3,
-        name: 'The Big Short',
-        image: '',
+        name: 'Rush Hour',
+        image: images.dummyRushHour,
         genre: 'Drama',
-        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+        description: 'A loyal and dedicated Hong Kong Inspector teams up with a reckless and loudmouthed L.A.P.D. detective to rescue the Chinese Consul\'s kidnapped daughter, while trying to arrest a dangerous crime lord along the way.',
         duration: '1h30min',
         direction: 'Vasilije Čabarkapa',
         actors: 'Neven Ivanović, Marko Marković',
-        rating: 8.2,
-        dateTime: '02.01.2023. 19:30',
+        rating: 7,
+        dateTime: '04.01.2023. 21:30',
         numberOfTickets: 220,
-        price: 5.00
+        price: 3.00
     }
 ]
 
