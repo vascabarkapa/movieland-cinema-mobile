23f73d0bff384d4882995d999db6a985eb2b4c6a
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2023-01-03 21:47:47 +0100

    Flatlist for repertory

--- screens/repertory/Repertory.js
+++ screens/repertory/Repertory.js
@@ -1,20 +1,76 @@
 import React from 'react';
-import {Text, View, StyleSheet} from "react-native";
-import {COLORS} from "../../constants";
+import {Text, View, StyleSheet, Image, FlatList, StatusBar} from "react-native";
+import {images} from "../../constants";
+import repertoryDB from "../../repertoryDB";
 
 const Repertory = () => {
+    const movies = ({item}) => {
+        return (
+            <View style={styles.movies}>
+                <View>
+                    <Image style={styles.movieImage} source={images.cinema}/>
+                </View>
+                <View>
+                    <Text style={styles.movieTitle}>
+                        {item.name}
+                    </Text>
+                    <Text style={styles.movieDescription} numberOfLines={2} ellipsizeMode='tail'>
+                        {item.description}
+                    </Text>
+                    <Text style={styles.moviePrice}>
+                        Price: {item.price}
+                    </Text>
+                    <Text style={styles.movieButton}>
+                        DETAILS
+                    </Text>
+                </View>
+            </View>
+        )
+    };
+
     return (
         <View style={styles.container}>
-            <Text>This is Repertory Page!</Text>
+            <Text>Ove sedmice na repertoaru:</Text>
+            <FlatList data={repertoryDB} renderItem={movies} keyExtractor={(item) => item.id}/>
+            {/*<StatusBar*/}
+            {/*    animated={true}*/}
+            {/*    backgroundColor="#61dafb"*/}
+            {/*/>*/}
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: COLORS.gray
-    }
-})
+        flex: 1
+    },
+    movies: {
+        flexDirection: 'column',
+    },
+    movieImage: {
+        height: 180,
+        width: '100%',
+    },
+    movieTitle: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    movieDescription: {
+        textAlign: 'center',
+        fontWeight: '600',
+        paddingTop: 5,
+    },
+    moviePrice: {
+        textAlign: 'center',
+        fontWeight: '600',
+        paddingTop: 5,
+    },
+    movieButton: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        paddingBottom: 15,
+        paddingTop: 5,
+    },
+});
 
 export default Repertory;
