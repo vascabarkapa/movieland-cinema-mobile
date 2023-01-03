855f37f0a6a57bc3e1b1972b5943c7b426b46738
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2023-01-03 23:58:00 +0100

    Added Dummy movies in constants

--- constants/images.js
+++ constants/images.js
@@ -1,7 +1,13 @@
 export const cinema = require('../assets/images/cinema_hall.jpg');
 export const welcomeScreen = require('../assets/images/welcome_screen.png');
+export const dummyRushHour = require('../assets/images/rush_hour.jpg');
+export const dummyRockyIII = require('../assets/images/rocky.jpeg');
+export const dummyTheBigShort = require('../assets/images/the_big_short.jpg');
 
 export default {
     cinema,
-    welcomeScreen
+    welcomeScreen,
+    dummyRushHour,
+    dummyRockyIII,
+    dummyTheBigShort
 }
