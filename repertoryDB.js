af9af0e113564eaedd89dd574a14ce2175255b28
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2023-01-03 21:47:18 +0100

    repertory dummy database

---
+++ repertoryDB.js
@@ -0,0 +1,46 @@
+const repertory = [
+    {
+        id: 1,
+        name: 'The Big Short',
+        image: '',
+        genre: 'Drama',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+        duration: '1h30min',
+        direction: 'Vasilije Čabarkapa',
+        actors: 'Neven Ivanović, Marko Marković',
+        rating: 8.2,
+        dateTime: '02.01.2023. 19:30',
+        numberOfTickets: 220,
+        price: 5.00
+    },
+    {
+        id: 2,
+        name: 'The Big Short',
+        image: '',
+        genre: 'Drama',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+        duration: '1h30min',
+        direction: 'Vasilije Čabarkapa',
+        actors: 'Neven Ivanović, Marko Marković',
+        rating: 8.2,
+        dateTime: '02.01.2023. 19:30',
+        numberOfTickets: 220,
+        price: 5.00
+    },
+    {
+        id: 3,
+        name: 'The Big Short',
+        image: '',
+        genre: 'Drama',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+        duration: '1h30min',
+        direction: 'Vasilije Čabarkapa',
+        actors: 'Neven Ivanović, Marko Marković',
+        rating: 8.2,
+        dateTime: '02.01.2023. 19:30',
+        numberOfTickets: 220,
+        price: 5.00
+    }
+]
+
+export default repertory;
