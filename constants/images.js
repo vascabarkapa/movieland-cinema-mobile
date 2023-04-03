608c1bc5228369065aaa17cd8f7c65e3a3120191
vascabarkapa <vascabarkapa@gmail.com>
2023-04-04 00:29:50 +0200

    added test movie pic

--- constants/images.js
+++ constants/images.js
@@ -4,6 +4,7 @@ export const dummyRushHour = require('../assets/images/rush_hour.jpg');
 export const dummyRockyIII = require('../assets/images/rocky.jpeg');
 export const dummyTheBigShort = require('../assets/images/the_big_short.jpg');
 export const everythingEverywhereAllAtOnce = require('../assets/images/everything_everywhere_all_at_once.png');
+export const testMovie = require('../assets/images/test_movie.png');
 
 export default {
     cinema,
@@ -11,5 +12,6 @@ export default {
     dummyRushHour,
     dummyRockyIII,
     dummyTheBigShort,
-    everythingEverywhereAllAtOnce
+    everythingEverywhereAllAtOnce,
+    testMovie
 }
